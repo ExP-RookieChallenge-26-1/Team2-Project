7f1d1fe5a90e18c30c22f13f37a409a621a93bc0
ajy-dev <ajy.dev@protonmail.ch>
2026-06-21 17:47:41 +0900

    Add animations

--- Assets/Gameplay/Ball/BallAnimation.cs
+++ Assets/Gameplay/Ball/BallAnimation.cs
@@ -4,14 +4,20 @@ public class BallAnimation
 {
 	private Animator animator;
 	private Ball ball;
+	private SpriteRenderer spriteRenderer;
 
 	private int isMovingUpHash;
+	private int attackHash;
+	private int cloneHash;
 
-	public BallAnimation(Animator animator, Ball ball)
+	public BallAnimation(Ball ball)
 	{
-		this.animator = animator;
 		this.ball = ball;
+		this.animator = ball.GetComponent<Animator>();
+		this.spriteRenderer = ball.GetComponent<SpriteRenderer>();
 		this.isMovingUpHash = Animator.StringToHash("IsMovingUp");
+		this.attackHash = Animator.StringToHash("Attack");
+		this.cloneHash = Animator.StringToHash("Clone");
 
 		if (this.animator == null)
 			Debug.LogError("BallAnimation: Animator를 찾을 수 없습니다!");
@@ -23,10 +29,29 @@ public class BallAnimation
 			return;
 
 		UpdateIdleUpDownState();
+		UpdateFlipState();
 	}
 
 	private void UpdateIdleUpDownState()
 	{
 		this.animator.SetBool(this.isMovingUpHash, this.ball.Physics.Velocity.y >= 0f);
+	}
+
+	public void TriggerClone()
+	{
+		this.animator.SetTrigger(this.cloneHash);
+	}
+
+	public void TriggerAttack()
+	{
+		this.animator.SetTrigger(this.attackHash);
+	}
+
+	private void UpdateFlipState()
+	{
+		if (this.ball.Physics.Velocity.x == 0f)
+			return;
+
+		this.spriteRenderer.flipX = this.ball.Physics.Velocity.x < 0f;
 	}
 }
